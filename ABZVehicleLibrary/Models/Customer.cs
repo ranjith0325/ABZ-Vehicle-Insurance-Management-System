208a7a46e846a7a0d9a4f8d9bbb4e2b703ad7beb
mani <manisms6942@gmail.com>
2024-12-19 12:26:05 +0530

    vehicle library cfreated

---
+++ ABZVehicleLibrary/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABZVehicleLibrary.Models;
+
+public partial class Customer
+{
+    public string CustomerId { get; set; } = null!;
+
+    public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+}
