7a3a65bf4af23695393b783a9ef3c81d2a88656b
mani <manisms6942@gmail.com>
2024-12-19 10:40:06 +0530

    Tables created

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿create database ABZAgentDB;
+create database ABZClaimDB;
+create database ABZCustomerDB;
+create database ABZPolicyDB;
+create database ABZProductDB;
+create database ABZProposalDB;
+create database ABZVehicleDB;
