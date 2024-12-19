0c022cacf3b3150fc97f36994239a7d3d06a31ce
mani <manisms6942@gmail.com>
2024-12-19 16:21:53 +0530

    WebApi created

---
+++ ABZVehicleWebApi/Controllers/VehicleController.cs
@@ -0,0 +1,93 @@
+﻿using ABZVehicleLibrary.Models;
+using ABZVehicleLibrary.Repos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABZVehicleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        IVehicleRepoAsync vehRepo;
+        public VehicleController(IVehicleRepoAsync repo)
+        {
+            vehRepo = repo;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            List<Vehicle> vehicles = await vehRepo.GetAllVehiclesAsync();
+            return Ok(vehicles);
+        }
+        [HttpGet("{regNo}")]
+        public async Task<ActionResult> GetOne(string regNo)
+        {
+            try
+            {
+                Vehicle vehicle = await vehRepo.GetVehicleAsync(regNo);
+                return Ok(vehicle);
+            }
+            catch (Exception ex)
+            {
+
+                return NotFound();
+            }
+        }
+
+        [HttpGet("ByCustomer/{customerId}")]
+        public async Task<ActionResult> GetByCustomer(string customerId)
+        {
+            try
+            {
+                List<Vehicle> vehicles = await vehRepo.GetVehiclesByCustomerAsync(customerId);
+                return Ok(vehicles);
+            }
+            catch (Exception ex)
+            {
+
+                return NotFound(ex.Message);
+            }
+        }
+        [HttpDelete("{regNo}")]
+        public async Task<ActionResult> Delete(string regNo)
+        {
+            try
+            {
+                await vehRepo.DeleteVehicleAsync(regNo);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> Insert(Vehicle vehicle)
+        {
+            try
+            {
+                await vehRepo.InsertVehicleAsync(vehicle);
+                return Created($"api/Vehicle/{vehicle.RegNo}",vehicle);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{regNo}")]
+        public async Task<ActionResult> Update(string regNo, Vehicle vehicle)
+        {
+            try
+            {
+                await vehRepo.UpdateVehicleAsync(regNo, vehicle);
+                return Ok(vehicle);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- ABZVehicleWebApi/Program.cs
+++ ABZVehicleWebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using ABZVehicleLibrary.Repos;
+
 namespace ABZVehicleWebApi
 {
     public class Program
@@ -13,6 +15,7 @@ namespace ABZVehicleWebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<IVehicleRepoAsync, EFVehicleRepoAsync>();
 
             var app = builder.Build();
 
