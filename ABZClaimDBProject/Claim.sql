453fe8e9a058fe3b95b6bc810ff7cf46a34c8767
GANJI01 <115651184+GANJI01@users.noreply.github.com>
2024-12-19 10:40:10 +0530

    Tables Created

---
+++ ABZClaimDBProject/Claim.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Claim]
+(
+	ClaimNo CHAR(10) NOT NULL PRIMARY KEY,
+	ClaimDate DATETIME,
+	PolicyNo CHAR(10) NOT NULL,
+	FOREIGN KEY (PolicyNo) REFERENCES Policy(PolicyNo),
+	IncidentDate DATETIME NOT NULL,
+	IncidentLocation VARCHAR(50) NOT NULL,
+	IncidentDescription VARCHAR(100),
+	ClaimAmount MONEY NOT NULL,
+	SurveyorName VARCHAR(30) NOT NULL,
+	SurveyorPhone CHAR(10) NOT NULL,
+	SurveyDate DATETIME,
+	SurveyDescription VARCHAR(100),
+	ClaimStatus CHAR(1) CHECK (ClaimStatus IN ('S', 'A', 'R', 'T')) NOT NULL,
+    
+);
---
+++ ABZClaimDBProject/Policy.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Policy]
+(
+	PolicyNo CHAR(10) NOT NULL PRIMARY KEY
+);
