3528b807adde5fe23ac0dd2d40ceb4d2c4788440
GANJI01 <115651184+GANJI01@users.noreply.github.com>
2024-12-19 12:34:32 +0530

    interface and class created

---
+++ ABZClaimsLibrary/RepoAsync/EFClaimRepoAsync.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABZClaimsLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ABZClaimsLibrary.RepoAsync
+{
+    public class EFClaimRepoAsync : IClaimRepoAsync
+    {
+        ABZClaimDBContext ctx = new ABZClaimDBContext();
+
+        public async Task DeleteClaimAsync(string claimNo)
+        {
+            Claim claim = await GetClaimByNoAsync(claimNo);
+            ctx.Claims.Remove(claim);
+            await ctx.SaveChangesAsync();
+        }
+
+        public async Task<List<Claim>> GetAllClaimAsync(string claimNo)
+        {
+            List<Claim> claims = await ctx.Claims.ToListAsync();
+            return claims;
+        }
+
+        public async Task<Claim> GetClaimByNoAsync(string claimNo)
+        {
+
+            try
+            {
+                Claim claim = await (from c in ctx.Claims
+                                     where c.ClaimNo == claimNo
+                                     select c).FirstAsync();
+                return claim;
+
+                if (claim == null)
+                {
+                    throw new KeyNotFoundException($"No claim found with ClaimNo: {claimNo}");
+                }
+                return claim;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Claim> GetClaimsByPolicyNoAsync(string policyNo)
+        {
+            try
+            {
+                Claim claim = await (from c in ctx.Claims
+                                     where c.PolicyNo == policyNo
+                                     select c).FirstAsync();
+                return claim;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        
+
+        public async Task InsertClaimAsync(Claim claim)
+        {
+            await ctx.Claims.AddAsync(claim);
+            await ctx.SaveChangesAsync();
+        }
+
+        public async Task UpdateClaimAsync(string claimNo,Claim claim)
+        {
+            Claim claim1 = await GetClaimByNoAsync(claimNo);
+            claim1.ClaimAmount = claim.ClaimAmount;
+            claim1.ClaimNo = claim.ClaimNo;
+            claim1.ClaimDate = claim.ClaimDate;
+            claim1.ClaimStatus = claim.ClaimStatus;
+            await ctx.SaveChangesAsync();
+        }
+    }
+}
---
+++ ABZClaimsLibrary/RepoAsync/IClaimRepoAsync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABZClaimsLibrary.Models;
+
+namespace ABZClaimsLibrary.RepoAsync
+{
+    public interface IClaimRepoAsync
+    {
+        Task InsertClaimAsync(Claim claim);
+        Task UpdateClaimAsync(string claimNo,Claim claim);
+        Task DeleteClaimAsync(string claimNo);
+        Task<Claim> GetClaimByNoAsync(string claimNo);
+        Task<Claim> GetClaimsByPolicyNoAsync(string policyNo);
+        Task<List<Claim>> GetAllClaimAsync(string claimNo);
+    }
+}
