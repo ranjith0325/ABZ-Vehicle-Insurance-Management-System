fd04ce154dbb097e21a251f81ac138702d522527
ranjith0325 <ranjithkumar3235@gmail.com>
2024-12-20 10:09:30 +0530

    Empty Mvc's Created

---
+++ ABZAgentMvcApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABZAgentMvcApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ABZPolicyMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABZPolicyMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
