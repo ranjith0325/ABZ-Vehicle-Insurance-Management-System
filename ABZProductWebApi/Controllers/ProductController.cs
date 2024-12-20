d5b5dd7d59cf3d1185585495ccd2e1b09d51e924
akshitha <bolluakshitha@gmail.com>
2024-12-20 23:06:19 +0530

    ProductController

---
+++ ABZProductWebApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using ABZProductLibrary.Models;
+using ABZProductLibrary.Repos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABZProductWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        IProductRepoAsync proRepo;
+        public ProductController(IProductRepoAsync repo)
+        {
+            proRepo= repo;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            List<Product> products = await proRepo.GetAllProductAsync();
+            return Ok(products);
+        }
+    }
+}
