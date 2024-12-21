c7da0d6ff7b622d2b5531bd32a7b85029ce727b5
mani <manisms6942@gmail.com>
2024-12-21 17:34:19 +0530

    Dateonly to date time

--- ABZProposalLibrary/Models/Proposal.cs
+++ ABZProposalLibrary/Models/Proposal.cs
@@ -13,9 +13,9 @@ public partial class Proposal
 
     public string CustomerID { get; set; } = null!;
 
-    public DateOnly FromDate { get; set; }
+    public DateTime FromDate { get; set; }
 
-    public DateOnly ToDate { get; set; }
+    public DateTime ToDate { get; set; }
 
     public decimal IDV { get; set; }
 
