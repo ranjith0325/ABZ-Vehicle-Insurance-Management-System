620aa3ca2e25b7c669a72d54a684091334d365ad
mani <manisms6942@gmail.com>
2024-12-21 17:22:42 +0530

    RegNO to RegNo

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,1 @@
-﻿create database ABZAgentDB;
-create database ABZClaimDB;
-create database ABZCustomerDB;
-create database ABZPolicyDB;
-create database ABZProductDB;
-create database ABZProposalDB;
-create database ABZVehicleDB;
+﻿
