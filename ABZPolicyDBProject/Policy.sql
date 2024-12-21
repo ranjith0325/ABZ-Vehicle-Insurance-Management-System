2d85f6853174f88a29f3a0cba9a83ac7ff710acb
mani <manisms6942@gmail.com>
2024-12-21 13:30:23 +0530

    proposal checked

--- ABZPolicyDBProject/Policy.sql
+++ ABZPolicyDBProject/Policy.sql
@@ -1,11 +1,10 @@
 ﻿CREATE TABLE [dbo].[Policy]
 (
 	PolicyNo char(10) PRIMARY KEY,
-	ProposalNo char(10) references Proposal(ProposalID),
+	ProposalNo char(10) references Proposal(ProposalNo),
 	NoClaimBonus money,
 	ReceiptNo char(5) not null,
 	ReceiptDate Datetime not null,
 	PaymentMode char(1) check (PaymentMode in ('C','Q','U','D')) not null,
 	Amount money 
-
 )
