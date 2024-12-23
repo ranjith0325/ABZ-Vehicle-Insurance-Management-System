a2fc52b34382faecd5c006b0e9065e289e6e15f0
mani <manisms6942@gmail.com>
2024-12-23 09:38:47 +0530

    mvc created

---
+++ ABZVehicleInsuranceMvcProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABZVehicleInsuranceMvcProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
