c61066696109d99f3b85cb4850b2bcc119cff551
akshitha <bolluakshitha@gmail.com>
2024-12-23 15:31:42 +0530

    Views Created product,productaddon,agent,customer.

---
+++ ABZVehicleInsuranceMvcProject/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Net.Http.Json;
+using ABZVehicleInsuranceMvcProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABZVehicleInsuranceMvcProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        static HttpClient client = new HttpClient() { BaseAddress = new Uri(" ") };
+        // GET: CustomerController
+        public async Task<ActionResult> Index()
+        {
+            List<Customer> customers = await client.GetFromJsonAsync<List<Customer>>("");
+            return View(customers);
+        }
+
+        // GET: CustomerController/Details/5
+        public async Task<ActionResult> Details(string customerId)
+        {
+            Customer customer = await client.GetFromJsonAsync<Customer>("" + customerId);
+            return View(customer);
+        }
+
+        // GET: CustomerController/Create
+        public async Task<ActionResult> Create(string customerId)
+        {
+            Customer customer = new Customer();
+            return View(customer);
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Customer customer)
+        {
+            try
+            {
+                await client.PostAsJsonAsync<Customer>("", customer);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public async Task<ActionResult> Edit(string customerId)
+        {
+            Customer customer = await client.GetFromJsonAsync<Customer>("" + customerId);
+            return View(customer);
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Customer/Edit/{customerId}")]
+        public async Task<ActionResult> Edit(string customerId, Customer customer)
+        {
+            try
+            {
+                await client.PutAsJsonAsync<Customer>("" + customerId, customer);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public async Task<ActionResult> Delete(string customerId)
+        {
+            Customer customer = await client.GetFromJsonAsync<Customer>("" + customerId);
+            return View(customer);
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Customer/Delete/{customerId}")]
+        public async Task<ActionResult> Delete(string customerId, IFormCollection collection)
+        {
+            try
+            {
+                await client.DeleteAsync("" + customerId);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ABZVehicleInsuranceMvcProject/Controllers/ProductAddonController.cs
@@ -0,0 +1,105 @@
+﻿using ABZVehicleInsuranceMvcProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ABZVehicleInsuranceMvcProject.Controllers;
+
+namespace ABZVehicleInsuranceMvcProject.Controllers
+{
+    public class ProductAddonController : Controller
+    {
+        // GET: ProductAddonController
+        static HttpClient client = new HttpClient() { BaseAddress = new Uri(" ") };
+        public async Task<ActionResult> Index()
+        {
+            List<ProductAddon> productaddons = await client.GetFromJsonAsync<List<ProductAddon>>("");
+            return View(productaddons);
+        }
+
+        // GET: ProductAddonController/Details/5
+        public async Task<ActionResult> Details(string addonId)
+        {
+            ProductAddon productaddon = await client.GetFromJsonAsync<ProductAddon>("" + addonId);
+            return View(productaddon);
+        }
+
+        // GET: ProductAddonController/Create
+        public ActionResult Create()
+        {
+            ProductAddon productaddon = new ProductAddon();
+            return View(productaddon);
+        }
+
+        // POST: ProductAddonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProductAddon productaddon)
+        {
+            try
+            {
+                await client.PostAsJsonAsync<ProductAddon>("", productaddon);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductAddonController/Edit/5
+        public async Task<ActionResult> Edit(string addonId)
+        {
+            ProductAddon productaddon = await client.GetFromJsonAsync<ProductAddon>("" + addonId);
+            return View(productaddon);
+        }
+
+        // POST: ProductAddonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("ProductAddon/Edit/{productaddonId}")]
+
+        public async Task<ActionResult> Edit(string addonId, ProductAddon productaddon)
+        {
+            try
+            {
+                await client.PostAsJsonAsync<ProductAddon>("" + addonId, productaddon);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductAddonController/Delete/5
+        [Route("ProductAddon/Delete/{addonID}")]
+
+        public async Task<ActionResult> Delete(string addonId)
+        {
+            ProductAddon productaddon = await client.GetFromJsonAsync<ProductAddon>("" + addonId);
+            return View(productaddon);
+        }
+
+        // POST: ProductAddonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("ProductAddon/Delete/{addonId}")]
+
+        public async Task<ActionResult> Delete(string addonId, IFormCollection collection)
+        {
+            try
+            {
+                await client.DeleteAsync("" + addonId);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public async Task<ActionResult> ByProduct(string productId)
+        {
+            List<ProductAddon> productAddons = await client.GetFromJsonAsync<List<ProductAddon>>("ByProduct/" + productId);
+            return View(productAddons);
+        }
+    }
+}
