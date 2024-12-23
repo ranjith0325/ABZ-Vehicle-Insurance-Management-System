4683da33bd1afa5e6f1d024c9f05fcf0bcf6d7a9
ranjith0325 <ranjithkumar3235@gmail.com>
2024-12-23 09:39:40 +0530

    data base name given as mani

--- ABZVehicleLibrary/Models/ABZVehicleDBContext.cs
+++ ABZVehicleLibrary/Models/ABZVehicleDBContext.cs
@@ -21,7 +21,7 @@ public partial class ABZVehicleDBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("data source=(localdb)\\MSSQLLocalDB; database=ABZVehicleDB; integrated security=true");
+        => optionsBuilder.UseSqlServer("data source=sqlsvrmani.database.windows.net; database=ABZVehicleDBProject; user id=mani; password=Root@1234");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
