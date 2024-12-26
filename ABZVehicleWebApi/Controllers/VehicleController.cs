fd78910e36588ebd45e2b7c49f7c2489f68ca7a9
mani <manisms6942@gmail.com>
2024-12-26 14:31:11 +0530

    k

--- ABZVehicleWebApi/Controllers/VehicleController.cs
+++ ABZVehicleWebApi/Controllers/VehicleController.cs
@@ -1,5 +1,6 @@
 ﻿using ABZVehicleLibrary.Models;
 using ABZVehicleLibrary.Repos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ namespace ABZVehicleWebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class VehicleController : ControllerBase
     {
         IVehicleRepoAsync vehRepo;
