e1a6f9e8e211131bc384e765fd95cfdc89588ace
mani <manisms6942@gmail.com>
2024-12-26 16:47:13 +0530

    customerquery library created

---
+++ ABZCustomerQueryLibrary/Models/CustomerQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABZCustomerQueryLibrary.Models;
+
+public partial class CustomerQuery
+{
+    public string QueryID { get; set; } = null!;
+
+    public string CustomerID { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime? QueryDate { get; set; }
+
+    public string? status { get; set; }
+
+    public virtual Customer Customer { get; set; } = null!;
+
+    public virtual ICollection<QueryResponse> QueryResponses { get; set; } = new List<QueryResponse>();
+}
