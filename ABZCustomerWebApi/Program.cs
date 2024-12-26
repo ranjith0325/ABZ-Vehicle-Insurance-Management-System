74ef10b8b9a4204ebd295a161e0d6ba6189dc590
akshitha <bolluakshitha@gmail.com>
2024-12-26 14:28:01 +0530

    agent claim customer product auth

--- ABZCustomerWebApi/Program.cs
+++ ABZCustomerWebApi/Program.cs
@@ -1,5 +1,8 @@
 
+using System.Text;
 using ABZCustomerLibrary.RepoAsync;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ABZCustomerWebApi
 {
@@ -16,6 +19,24 @@ namespace ABZCustomerWebApi
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<ICustomerRepoAsync, EFCustomerAsync>();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = "https://www.team2.com",
+                    ValidAudience = "https://www.team2.com",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("My name is Bond, James Bond the great"))
+                };
+            });
 
             var app = builder.Build();
 
@@ -25,7 +46,7 @@ namespace ABZCustomerWebApi
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
