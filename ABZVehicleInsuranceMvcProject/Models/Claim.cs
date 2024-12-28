f04b8bc374bb6cd69cf0312cea74ce206e8e9be2
mani <manisms6942@gmail.com>
2024-12-28 17:47:16 +0530

    all tables updated in mvc

--- ABZVehicleInsuranceMvcProject/Models/Claim.cs
+++ ABZVehicleInsuranceMvcProject/Models/Claim.cs
@@ -1,29 +1,34 @@
-﻿namespace ABZVehicleInsuranceMvcProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ABZVehicleInsuranceMvcProject.Models
 {
     public class Claim
     {
+        [RegularExpression(@"\w{10}", ErrorMessage = "Claim Number must be 10 characters")]
         public string ClaimNo { get; set; } = null!;
 
         public DateTime? ClaimDate { get; set; }
-
+        [Required]
         public string PolicyNo { get; set; } = null!;
-
+        [Required]
         public DateTime IncidentDate { get; set; }
-
+        [Required]
         public string IncidentLocation { get; set; } = null!;
 
         public string? IncidentDescription { get; set; }
-
+        [Required]
         public decimal ClaimAmount { get; set; }
+        [Required]
 
         public string SurveyorName { get; set; } = null!;
 
+        [RegularExpression(@"\d{10}", ErrorMessage = "Phone number must be 10 digits")]
         public string SurveyorPhone { get; set; } = null!;
 
         public DateTime? SurveyDate { get; set; }
 
         public string? SurveyDescription { get; set; }
-
+        [RegularExpression(@"^[sartSART]{1}$", ErrorMessage = "Please enter only 'S' or 'A' or 'R' or 'T'")]
         public string ClaimStatus { get; set; } = null!;
 
      //   public virtual Policy? PolicyNoNavigation { get; set; } = null!;
--- ABZVehicleInsuranceMvcProject/Models/Product.cs
+++ ABZVehicleInsuranceMvcProject/Models/Product.cs
@@ -1,19 +1,24 @@
-﻿namespace ABZVehicleInsuranceMvcProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ABZVehicleInsuranceMvcProject.Models
 {
     public class Product
     {
+        [RegularExpression(@"\w{10}", ErrorMessage = "ProductID must be 10 characters")]
+
         public string ProductID { get; set; } = null!;
-
+        [Required]
         public string ProductName { get; set; } = null!;
-
+        [Required]
         public string ProductDescription { get; set; } = null!;
+        [RegularExpression(@"\w{20}", ErrorMessage = "ProductUIN must be 20 characters")]
 
         public string ProductUIN { get; set; } = null!;
-
+        [RegularExpression(@"^(public car|private car)$", ErrorMessage = "Please enter either 'public car' or 'private car'.")]
         public string InsuredInterests { get; set; } = null!;
-
+        [Required]
         public string PolicyCoverage { get; set; } = null!;
 
-        public virtual ICollection<ProductAddon> ProductAddons { get; set; } = new List<ProductAddon>();
+     //   public virtual ICollection<ProductAddon> ProductAddons { get; set; } = new List<ProductAddon>();
     }
 }
