7d735cd1958e61ba9ca1e6e9e88250f48f96b032
mani <manisms6942@gmail.com>
2024-12-28 14:23:17 +0530

    k

--- ABZCustomerWebApi/Controllers/CustomerController.cs
+++ ABZCustomerWebApi/Controllers/CustomerController.cs
@@ -48,9 +48,9 @@ namespace ABZCustomerWebApi.Controllers
                // await client.PostAsJsonAsync("http://localhost:5273/api/Proposal/Customer", new {CustomerID=customer.CustomerID});
               //  await client.PostAsJsonAsync("http://localhost:5083/api/Vehicle/Customer",new { CustomerID = customer.CustomerID });
               //  await client.PostAsJsonAsync("http://localhost:5091/api/CustomerQuery/Customer", new { CustomerID = customer.CustomerID });
-                await client.PostAsJsonAsync("http://abzvehiclewebapi-akshitha.azurewebsites.net/api/Vehicle/Customer", new { CustomerId = customer.CustomerID });
-                await client.PostAsJsonAsync("http://abzproposalwebapi-akshitha.azurewebsites.net/api/Proposal/Customer", new { CustomerId = customer.CustomerID });
-                await client.PostAsJsonAsync("http://abzcustomerquerywebapi.azurewebsites.net/api/CustomerQuery/Customer", new { CustomerID = customer.CustomerID });
+                await client.PostAsJsonAsync("http://abzvehiclewebapi-akshitha.azurewebsites.net/api/Vehicle/Customer", new { CustomerID = customer.CustomerID });
+                await client.PostAsJsonAsync("http://abzproposalwebapi-akshitha.azurewebsites.net/api/Proposal/Customer", new { CustomerID = customer.CustomerID });
+                await client.PostAsJsonAsync("http://abzcustomerquerywebapi-akshitha.azurewebsites.net/api/CustomerQuery/Customer", new { CustomerID = customer.CustomerID });
 
 
                 return Created($"api/Customer/{customer.CustomerID}", customer);
