211beb46a05b3f40f49a416061b3f96e271923fc
akshitha <bolluakshitha@gmail.com>
2024-12-28 10:40:20 +0530

    customerquery published

--- ABZCustomerQueryLibrary/Models/ABZCustomerQueryDBContext.cs
+++ ABZCustomerQueryLibrary/Models/ABZCustomerQueryDBContext.cs
@@ -25,7 +25,8 @@ public partial class ABZCustomerQueryDBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("data source=(localdb)\\MSSQLLocalDB; database=ABZCustomerQueryDB; integrated security=true");
+       // => optionsBuilder.UseSqlServer("data source=(localdb)\\MSSQLLocalDB; database=ABZCustomerQueryDB; integrated security=true");
+          => optionsBuilder.UseSqlServer("data source=sqlsvrakshitha.database.windows.net; database=ABZCustomerQueryDB; user id=akshitha; password=Root@1234");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
